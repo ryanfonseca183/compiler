4457bd0708f981ae52002fe32d0d8a3b08e2c686
Ryan William Fonseca <ryanfonseca183@gmail.com>
2023-11-15 16:26:06 -0300

    Initial commit

---
+++ lexico.py
@@ -0,0 +1,73 @@
+"""
+
+ Linguagem Toy
+
+    Gramatica::
+
+    F* --> C F | C
+    C  --> A | R | P
+    A --> ident = E ;
+    R --> read ( ident ) ;
+    P --> print ( ident ) ;
+
+    E --> M Rs
+    Rs --> + M Rs | lambda
+    M --> Op Rm
+    Rm --> * Op Rm | lambda
+    Op --> ( E ) | num
+
+    Tokens::
+
+    IDENT ATRIB READ PTOVIRG PRINT ADD MULT OPENPAR CLOSEPAR NUM ERROR FIMARQ
+
+    Comentarios::
+
+    iniciam com # ate o fim da linha
+
+"""
+
+from os import path
+
+class TypeToken:
+    ID = (1, 'ID')
+    READ = (2, 'READ')
+    WRITE = (3, 'WRITE')
+    CTE = (4, 'CONSTANTE'),
+    CADEIA = (5, 'CADEIA'),
+    ATRIB = (6, '=')
+    OPREL = (7, 'RELACIONAL')
+    OPAD = (8, 'ADIÇÃO')
+    OPMULT = (9, '*')
+    OPNEG = (10, '!')
+    PVIRG = (11, ';')
+    DPONTOS = (12, ':')
+    VIRG = (13, ',')
+    ABREPAR = (14, '(')
+    FECHAPAR = (15, ')')
+    ABRECH = (16, '{')
+    FECHACH = (17, '}')
+    ERROR = (18, 'ERROR')
+    EOF = (19, 'END OF FILE')
+    PROGRAM = (20, 'PROGRAM'),
+    VAR = (21, 'VAR'),
+    INT = (22, 'INT')
+    REAL = (23, 'REAL'),
+    CHAR = (24, 'CHAR'),
+    IF = (25, 'IF'),
+    ELSE = (26, 'ELSE'),
+    WHILE = (27, 'WHILE'),
+    READ = (28, 'READ'),
+    FALSE = (29, 'FALSE'),
+    TRUE = (30, 'TRUE')
+
+class Token:
+    def __init__(self, typeToken, lexeme, line):
+        (const, label) = typeToken
+        self.type = typeToken
+        self.lexeme = lexeme
+        self.const = const
+        self.label = label
+        self.line = line
+
+if __name__== "__main__":
+   print(TipoToken.IDENT)
